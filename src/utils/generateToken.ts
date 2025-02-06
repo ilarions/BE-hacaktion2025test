9b1d3224a0c0f3f90acd525b1ef627445cd162e7
artem <artemk2504@gmail.com>
2025-02-06 16:04:18 +0200

    Update generateToken.ts

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,1 +1,16 @@
-export const generateToken = () => {};
+import jwt from 'jsonwebtoken';
+
+export interface IGenerateToken {
+  id: string;
+  secret: string;
+  time: string;
+}
+
+export const generateToken = (
+  id: string,
+  secret: string,
+  time: string,
+): string => {
+  const token = jwt.sign({ id: id }, secret, { expiresIn: time });
+  return token;
+};
